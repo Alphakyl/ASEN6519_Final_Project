4cfde9c9050de04ff8bed4eafc84700ce197256e
Alphakyl <kyle.harlow@colorado.edu>
2020-05-07 00:29:57 -0600

    called ekf class from ekf.py instead for consistency/redundancy

--- scripts/gsf.py
+++ scripts/gsf.py
@@ -5,6 +5,7 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 from std_msgs.msg import *
+from ekf import EKF
 import numpy as np
 import math
 import tf
@@ -15,102 +16,102 @@ from scipy import stats
 # Global Variables
 initilization = True
 
-class EKF:
-	def __init__(self,x_0,P_0):
-		# Define x_0
-        self.x_hat_k_plus_one_plus = x_0
+# class EKF:
+# 	def __init__(self,x_0,P_0):
+# 		# Define x_0
+#         self.x_hat_k_plus_one_plus = x_0
 
-		# Tunables: Q_t and P_0
-		sigma_x = 0.05 # m^2
-		sigma_y = 0.05 # m^2
-		sigma_theta = 0.01 # rad
-		sigma_x_dot = 0.5 # m^2/s
-		sigma_y_dot = 0.5 # m^2/s
-		self.Q_t = np.array([[sigma_x^2, 0., 0., 0., 0.],[0. sigma_y^2, 0., 0., 0.],[0., 0., sigma_theta^2, 0., 0.],[0., 0., 0., sigma_x_dot, 0.],[0., 0., 0., 0., sigma_y_dot]])
+# 		# Tunables: Q_t and P_0
+# 		sigma_x = 0.05 # m^2
+# 		sigma_y = 0.05 # m^2
+# 		sigma_theta = 0.01 # rad
+# 		sigma_x_dot = 0.5 # m^2/s
+# 		sigma_y_dot = 0.5 # m^2/s
+# 		self.Q_t = np.array([[sigma_x^2, 0., 0., 0., 0.],[0. sigma_y^2, 0., 0., 0.],[0., 0., sigma_theta^2, 0., 0.],[0., 0., 0., sigma_x_dot, 0.],[0., 0., 0., 0., sigma_y_dot]])
 		
-        # Define P_0
-        self.P_k_plus_one_plus = P_0
+#         # Define P_0
+#         self.P_k_plus_one_plus = P_0
 
-		# Estimated by sensor outputs
-		sigma_r_x = 0.005 # m^2
-		sigma_r_y = 0.005 # m^2
-		sigma_r_theta = 0.005 # rad
-		sigma_r_x_dot = 0.1 # m^2/s
-		sigma_r_y_dot = 0.1 # m^2/s
-		self.R_t = np.array([[sigma_r_x^2, 0., 0.],[0., sigma_r_y^2, 0.],[0., 0., sigma_r_theta^2]])
-		self.R_t_vel = np.array([[sigma_r_x^2, 0., 0., 0., 0.],[0. sigma_r_y^2, 0., 0., 0.],[0., 0., sigma_r_theta^2, 0., 0.],[0., 0., 0., sigma_r_x_dot, 0.],[0., 0., 0., 0., sigma_r_y_dot]])
+# 		# Estimated by sensor outputs
+# 		sigma_r_x = 0.005 # m^2
+# 		sigma_r_y = 0.005 # m^2
+# 		sigma_r_theta = 0.005 # rad
+# 		sigma_r_x_dot = 0.1 # m^2/s
+# 		sigma_r_y_dot = 0.1 # m^2/s
+# 		self.R_t = np.array([[sigma_r_x^2, 0., 0.],[0., sigma_r_y^2, 0.],[0., 0., sigma_r_theta^2]])
+# 		self.R_t_vel = np.array([[sigma_r_x^2, 0., 0., 0., 0.],[0. sigma_r_y^2, 0., 0., 0.],[0., 0., sigma_r_theta^2, 0., 0.],[0., 0., 0., sigma_r_x_dot, 0.],[0., 0., 0., 0., sigma_r_y_dot]])
 
-	def jacobian(self,x,u,delta_t):
-		s = np.sqrt([x[3]^2+x[4]^2])
-		F_tilde = np.identity(5)
-		A_tilde = np.zeros(5)
-		A_tilde[0,2] = -s*math.sin(x[2])
-		A_tilde[0,2] = s*math.cos(x[2])
-		if(s > 0):
-			A_tilde[1,4] = x[4]*cos(x[3])/s
-			A_tilde[1,5] = x[5]*cos(x[3])/s
-			A_tilde[2,4] = x[4]*sin(x[3])/s
-			A_tilde[2,5] = x[5]*sin(x[3])/s
+# 	def jacobian(self,x,u,delta_t):
+# 		s = np.sqrt([x[3]^2+x[4]^2])
+# 		F_tilde = np.identity(5)
+# 		A_tilde = np.zeros(5)
+# 		A_tilde[0,2] = -s*math.sin(x[2])
+# 		A_tilde[0,2] = s*math.cos(x[2])
+# 		if(s > 0):
+# 			A_tilde[1,4] = x[4]*cos(x[3])/s
+# 			A_tilde[1,5] = x[5]*cos(x[3])/s
+# 			A_tilde[2,4] = x[4]*sin(x[3])/s
+# 			A_tilde[2,5] = x[5]*sin(x[3])/s
 		
-		F_tilde = np.identity(5)+A_tilde*delta_t
-		return F_tilde
+# 		F_tilde = np.identity(5)+A_tilde*delta_t
+# 		return F_tilde
 
-	def f(self,x,u,delta_t):
-		s = np.srqt([x[3]^2+x[4]^2])
-		x_hat_k_plus_one_minus = np.zeros(shape(x))
-		x_hat_k_plus_one_minus[0] = x[0] + s*cos(x[2])*delta_t
-		x_hat_k_plus_one_minus[1] = x[1] + s*sin(x[2])*delta_t
-		x_hat_k_plus_one_minus[2] = x[2] + u[0]*delta_t
-		x_hat_k_plus_one_minus[2] = np.unwrap(x_hat_k_plus_one_minus[2])
-		x_hat_k_plus_one_minus[3] = x[3] + u[1]*delta_t
-		x_hat_k_plus_one_minus[4] = x[4] + u[2]*delta_t
-		return x_hat_k_plus_one_minus
+# 	def f(self,x,u,delta_t):
+# 		s = np.srqt([x[3]^2+x[4]^2])
+# 		x_hat_k_plus_one_minus = np.zeros(shape(x))
+# 		x_hat_k_plus_one_minus[0] = x[0] + s*cos(x[2])*delta_t
+# 		x_hat_k_plus_one_minus[1] = x[1] + s*sin(x[2])*delta_t
+# 		x_hat_k_plus_one_minus[2] = x[2] + u[0]*delta_t
+# 		x_hat_k_plus_one_minus[2] = np.unwrap(x_hat_k_plus_one_minus[2])
+# 		x_hat_k_plus_one_minus[3] = x[3] + u[1]*delta_t
+# 		x_hat_k_plus_one_minus[4] = x[4] + u[2]*delta_t
+# 		return x_hat_k_plus_one_minus
 
-	def h(self,x,delta_t, vel):
-		if vel:
-			y = x
-		else:
-			y = x[0:2]
-		return y		
+# 	def h(self,x,delta_t, vel):
+# 		if vel:
+# 			y = x
+# 		else:
+# 			y = x[0:2]
+# 		return y		
 
-	def prediction(self,u,delta_t):
-		self.x_hat_k_plus_one_minus = self.f(self.x_hat_k_plus_one_plus,u,delta_t)
-		F_tilde = self.jacobian(self.x_hat_k_plus_one_plus,u,delta_t)
-		self.P_k_plus_one_minus = np.dot(F_tilde,np.dot(self.P_k_plus_one_plus,F_tilde.T)) + self.Q_t
+# 	def prediction(self,u,delta_t):
+# 		self.x_hat_k_plus_one_minus = self.f(self.x_hat_k_plus_one_plus,u,delta_t)
+# 		F_tilde = self.jacobian(self.x_hat_k_plus_one_plus,u,delta_t)
+# 		self.P_k_plus_one_minus = np.dot(F_tilde,np.dot(self.P_k_plus_one_plus,F_tilde.T)) + self.Q_t
 		
-	def correction(self,y,delta_t):
-		if y.size() == 5:
-            vel = 1
-            R = R_t_vel
-            H = np.identity(5)
-		else:
-            vel = 0
-            R = R_t
-            H = np.array([[1., 0., 0., 0., 0.],
-						[0., 1., 0., 0., 0.],
-						[0., 0., 1., 0., 0.]])
-		self.y_hat_k_plus_one_minus = self.h(self.x_hat_k_plus_one_minus,delta_t,vel)
-		e_y_k_plus_one = y-self.y_hat_k_plus_one_minus
-		C_xy = np.dot(self.P_k_plus_one_minus,H.T)
-		self.S_k_plus_one = np.dot(H,C_xy)+R
-		S_k_plus_one_inv = np.linalg.inv(S_k_plus_one)
-		K = np.dot(C_xy,S_k_plus_one_inv)
-		self.x_hat_k_plus_one_plus = x+np.dot(K,e_y_k_plus_one)
-		temp_4 = np.dot(K,H)
-		temp_5 = np.identity(temp_4.shape)-temp_4
-		self.P_k_plus_one_plus = np.dot(temp_5,P)
+# 	def correction(self,y,delta_t):
+# 		if y.size() == 5:
+#             vel = 1
+#             R = R_t_vel
+#             H = np.identity(5)
+# 		else:
+#             vel = 0
+#             R = R_t
+#             H = np.array([[1., 0., 0., 0., 0.],
+# 						[0., 1., 0., 0., 0.],
+# 						[0., 0., 1., 0., 0.]])
+# 		self.y_hat_k_plus_one_minus = self.h(self.x_hat_k_plus_one_minus,delta_t,vel)
+# 		e_y_k_plus_one = y-self.y_hat_k_plus_one_minus
+# 		C_xy = np.dot(self.P_k_plus_one_minus,H.T)
+# 		self.S_k_plus_one = np.dot(H,C_xy)+R
+# 		S_k_plus_one_inv = np.linalg.inv(S_k_plus_one)
+# 		K = np.dot(C_xy,S_k_plus_one_inv)
+# 		self.x_hat_k_plus_one_plus = x+np.dot(K,e_y_k_plus_one)
+# 		temp_4 = np.dot(K,H)
+# 		temp_5 = np.identity(temp_4.shape)-temp_4
+# 		self.P_k_plus_one_plus = np.dot(temp_5,P)
     
-    def get_x_hat_k_plus_one_plus():
-        return self.x_hat_k_plus_one_plus
+#     def get_x_hat_k_plus_one_plus():
+#         return self.x_hat_k_plus_one_plus
     
-    def get_y_hat_plus_one_minus():
-        return self.y_hat_k_plus_one_minus
+#     def get_y_hat_plus_one_minus():
+#         return self.y_hat_k_plus_one_minus
     
-    def get_P_k_plus_one_plus():
-        return self.P_k_plus_one_plus
+#     def get_P_k_plus_one_plus():
+#         return self.P_k_plus_one_plus
     
-    def get_S_k_plus_one():
-        return self.S_k_plus_one
+#     def get_S_k_plus_one():
+#         return self.S_k_plus_one
 
 
 class GSF:
